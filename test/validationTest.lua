d5d6c66c3768734a798bbed23b729355bc0895f6
boniaslasher <AMikhniev@luxoft.com>
2016-02-26 16:06:51 +0200

    Beautify validationTest

--- test/validationTest.lua
+++ test/validationTest.lua
@@ -18,13 +18,25 @@ local json_hmi_tbl = { numTicks = 7, position = 6, sliderHeader ="sliderHeader",
 local json_mob_tbl = {success = true, resultCode = {"SUCCESS"}}
 
 local _res, _err = validator.validate_hmi_request('UI.Slider', json_hmi_tbl)
-if (not _res) then print("validate_hmi_request:"..tostring(_res).." ==> ".._err) else print ("validate_hmi_request:"..tostring(_res)) end
+if (not _res) then
+  print("validate_hmi_request:"..tostring(_res).." ==> ".._err)
+else
+  print ("validate_hmi_request:"..tostring(_res))
+end
 
 _res, _err = validator.validate_mobile_response('Slider',json_mob_tbl,true)
-if (not _res) then print("validate_mobile_response:"..tostring(_res).." ==> ".._err) else print ("validate_mobile_response:"..tostring(_res)) end
+if (not _res) then
+  print("validate_mobile_response:"..tostring(_res).." ==> ".._err)
+else
+  print ("validate_mobile_response:"..tostring(_res))
+end
 
 _res, _err = validator.validate_mobile_response( 'Slider',{ success = 'true', resultCode = {"SUCCESS"} },true)
-if (not _res) then print("validate_mobile_response:"..tostring(_res).." ==> ".._err) else print ("validate_mobile_response:"..tostring(_res)) end
+if (not _res) then
+  print("validate_mobile_response:"..tostring(_res).." ==> ".._err)
+else
+  print ("validate_mobile_response:"..tostring(_res))
+end
 
 _res, _err = validator.validate_mobile_response( "WrongFunctionName", { success = false, resultCode = "INVALID_DATA", info = nil })
 if (not _res) then
@@ -34,19 +46,39 @@ else
 end
 
 _res, _err = validator.validate_hmi_notification( 'BasicCommunication.OnSystemRequest', { requestType = {"PROPRIETARY"}})
-if (not _res) then print("validate_hmi_notification:"..tostring(_res).." ==> ".._err) else print ("validate_hmi_notification:"..tostring(_res)) end
+if (not _res) then
+  print("validate_hmi_notification:"..tostring(_res).." ==> ".._err)
+else
+  print ("validate_hmi_notification:"..tostring(_res))
+end
 
 _res, _err = validator.validate_hmi_notification( 'BasicCommunication.OnSystemRequest', { urlSchema = "default", fileName = "fileName"}, true)
-if (not _res) then print("validate_hmi_notification:"..tostring(_res).." ==> ".._err) else print ("validate_hmi_notification:"..tostring(_res)) end
+if (not _res) then
+  print("validate_hmi_notification:"..tostring(_res).." ==> ".._err)
+else
+  print ("validate_hmi_notification:"..tostring(_res))
+end
 
 _res, _err = validator.validate_mobile_notification( "OnHMIStatus", {hmiLevel = "FULL"})
-if (not _res) then print("validate_mobile_notification:"..tostring(_res).." ==> ".._err) else print ("validate_modile_notification:"..tostring(_res)) end
+if (not _res) then
+  print("validate_mobile_notification:"..tostring(_res).." ==> ".._err)
+else
+  print ("validate_modile_notification:"..tostring(_res))
+end
 
 _res, _err = validator.json_validate(json_mob_tbl, {resultCode = {"SUCCESS"} })
-if (not _res) then print("json_validate:"..tostring(_res).." ==> ".._err) else print ("json_validate:"..tostring(_res)) end
+if (not _res) then
+  print("json_validate:"..tostring(_res).." ==> ".._err)
+else
+  print ("json_validate:"..tostring(_res))
+end
 
 _res, _err = validator.json_validate(json_mob_tbl, { success = 'true', resultCode = {"SUCCESS"} } )
-if (not _res) then print("json_validate:"..tostring(_res).." ==> ".._err) else print ("json_validate:"..tostring(_res)) end
+if (not _res) then
+  print("json_validate:"..tostring(_res).." ==> ".._err)
+else
+  print ("json_validate:"..tostring(_res))
+end
 
 local _res, _err = validator.validate_mobile_request('PerformInteraction',
   { initialText = "initialText",
