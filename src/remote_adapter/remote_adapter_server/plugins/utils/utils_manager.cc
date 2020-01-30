94ae76a5ad6e9e56c3a40bfd23f9207be48d3dca
sniukalov <sniukaov@luxoft.com>
2020-01-30 16:35:24 +0200

    fixup! Add Remote Server

--- src/remote_adapter/remote_adapter_server/plugins/utils/utils_manager.cc
+++ src/remote_adapter/remote_adapter_server/plugins/utils/utils_manager.cc
@@ -583,7 +583,7 @@ std::string UtilsManager::GetFileContent(const std::string &file_path,
   file_len = ftell(file);
   fclose(file);
 
-  offset = (read + offset) == file_len ? error_codes::READ_EOF : read + offset;
+  offset = (read + offset) == file_len ? 0 : read + offset;
 
   std::string file_content(buffer, read);
 
