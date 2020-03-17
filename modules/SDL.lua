bf264c8fc6d8ed2bc21c7038f5eb6a8262b80945
Dmytro Boltovskyi (GitHub) <dboltovskyi@luxoft.com>
2020-03-17 15:58:58 -0400

    Set default WES support to OFF (#197)

--- modules/SDL.lua
+++ modules/SDL.lua
@@ -249,7 +249,7 @@ local function getDefaultBuildOptions()
   local options = { }
   options.remoteControl = { sdlBuildParameter = "REMOTE_CONTROL", defaultValue = "ON" }
   options.extendedPolicy = { sdlBuildParameter = "EXTENDED_POLICY", defaultValue = "PROPRIETARY" }
-  options.webSocketServerSupport =  { sdlBuildParameter = "BUILD_WEBSOCKET_SERVER_SUPPORT", defaultValue = "ON" }
+  options.webSocketServerSupport =  { sdlBuildParameter = "BUILD_WEBSOCKET_SERVER_SUPPORT", defaultValue = "OFF" }
   return options
 end
 
