4bb40a7decc38fea3ab06ea4230626557ec6b87d
Jacob Keeler <jacob.keeler@livioradio.com>
2021-08-27 15:07:28 -0400

    Remove query type from SendHandshakeData event matcher (#237)
    * Remove query type from SendHandshakeData event matcher
    
    Also change response to proper query type

--- modules/services/control_service.lua
+++ modules/services/control_service.lua
@@ -33,7 +33,6 @@ local function performStartServiceHandshake(controlService)
         return data.frameType ~= constants.FRAME_TYPE.CONTROL_FRAME
           and data.serviceType == constants.SERVICE_TYPE.CONTROL
           and data.sessionId == controlService.session.sessionId.get()
-          and data.rpcType == constants.BINARY_RPC_TYPE.NOTIFICATION
           and data.rpcFunctionId == constants.BINARY_RPC_FUNCTION_ID.HANDSHAKE
       end
 
@@ -46,7 +45,7 @@ local function performStartServiceHandshake(controlService)
             frameInfo = 0,
             serviceType = constants.SERVICE_TYPE.CONTROL,
             encryption = false,
-            rpcType = constants.BINARY_RPC_TYPE.NOTIFICATION,
+            rpcType = constants.BINARY_RPC_TYPE.RESPONSE,
             rpcFunctionId = constants.BINARY_RPC_FUNCTION_ID.HANDSHAKE,
             rpcCorrelationId = data.rpcCorrelationId,
             binaryData = dataToSend
